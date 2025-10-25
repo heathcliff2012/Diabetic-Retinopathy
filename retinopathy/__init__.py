e40db6f455da1b1170e62a4301fe43f041235149
H Navaneeth Dange <hndange@gmail.com>
2025-10-25 22:30:06 +0530

    created sqlLite database

--- retinopathy/__init__.py
+++ retinopathy/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -12,5 +13,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+migrate = Migrate(app, db)
 
 from retinopathy import routes
