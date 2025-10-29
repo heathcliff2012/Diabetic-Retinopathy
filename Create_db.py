1b613cb30a3093855d130265993fc01f90d09d4f
H Navaneeth Dange <hndange@gmail.com>
2025-10-29 18:21:41 +0530

    up

--- Create_db.py
+++ Create_db.py
@@ -1,7 +1,11 @@
-from retinopathy import app, db  # Change 'your_app_file' to the name of your .py file
+from retinopathy import app, db 
+from retinopathy.modules import Patient
+ # Change 'your_app_file' to the name of your .py file
 
 with app.app_context():
     db.create_all()
+    print(Patient.query.all())
 
 print("Database tables created.")
+
 
