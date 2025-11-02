039a5a25c286e622abb3a3283eb955cd1b9ffe9f
Bmo-273 <devshankarsingh40@gmail.com>
2025-11-02 16:57:30 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from retinopathy import app
-
+import dotenv
+dotenv.load_dotenv('.env')
+port = dotenv.get_key('.env', 'PORT') or 5000
 
 if __name__ == '__main__':
     app.run(debug=True)
--- retinopathy/__init__.py
+++ retinopathy/__init__.py
@@ -9,7 +9,6 @@ dotenv.load_dotenv('.env')
 
 app = Flask(__name__)
 
-port = dotenv.get_key('.env', 'PORT') or 5000
 app.config['SECRET_KEY'] = dotenv.get_key('.env', 'SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = dotenv.get_key('.env', 'SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
