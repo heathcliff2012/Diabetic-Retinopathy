f00e5fbffc33f643913ea9b811dba1749625906b
Bmo-273 <shaijo0320@gmail.com>
2025-11-03 15:53:17 +0530

    up

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from retinopathy import app
-from flask import Flask
-from flask import render_template
 
-
-# This is the object Gunicorn looks for
-app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
+if __name__ == '__main__':
+    app.run()
