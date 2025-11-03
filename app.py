b25036a7f835c252e544f5f7da6c4f39a4841470
Bmo-273 <devshankarsingh40@gmail.com>
2025-11-03 08:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ except Exception as e:
 
 import os
 
-
-port = int(os.environ.get("PORT", 8080))
 debug = os.environ.get("DEBUG", "").lower() in ("1", "true", "yes")
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(host="0.0.0.0:8080" debug=debug)
