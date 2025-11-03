b676b2a788991d49c930a1510f17b265bf77e096
Bmo-273 <devshankarsingh40@gmail.com>
2025-11-03 07:45:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from retinopathy import app
+try:
+    from retinopathy import app
+except Exception as e:
+    import sys
+    print("Failed to import 'app' from 'retinopathy' package:", e, file=sys.stderr)
+    raise
+
 import os
 
 
 port = int(os.environ.get("PORT", 8080))
+debug = os.environ.get("DEBUG", "").lower() in ("1", "true", "yes")
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=debug)
